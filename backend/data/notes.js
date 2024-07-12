1022bedbdd07008d4f572562b567b7bf58b00d20
Miftachul Ulum <113278752+MiftachulUlum@users.noreply.github.com>
2024-07-12 17:30:56 +0700

    Add files via upload

---
+++ backend/data/notes.js
@@ -0,0 +1,7 @@
+const notes = [
+    { id: 1, title: "Catatan 1", body: "Ini adalah catatan pertama." },
+    { id: 2, title: "Catatan 2", body: "Ini adalah catatan kedua." },
+  ];
+  
+  module.exports = notes;
+  
